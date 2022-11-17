7dbedf1fcd69f7e0d37cd734cde3fb3b46b6c6a8
Raamfox <rami.okal@kyh.student.se>
2022-11-17 19:10:16 +0100

    small addition

--- main.py
+++ main.py
@@ -21,9 +21,10 @@ def main():
         print("\n")
         in_num = input("Pick a program or write stop to exit: ")
         if in_num == "stop":
-            running = False
+            break
             # Takes the users chosen channel and gets its description and schedule
         print(first_page["channels"][int(in_num)]["tagline"])
+        print("Here is the link to the Radio!")
         print(first_page["channels"][int(in_num)]["siteurl"])
         schedule_url_dict = first_page["channels"][int(in_num)]["scheduleurl"]
 
